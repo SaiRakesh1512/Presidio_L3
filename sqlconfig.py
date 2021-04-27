ce3ab56a311e860b04ca615f6c57b1576b147640
SaiRakesh1512 <sai.rakesh820@gmail.com>
2021-04-27 08:29:08 +0530

    addition of table commands

--- sqlconfig.py
+++ sqlconfig.py
@@ -9,3 +9,9 @@ def connect():
 
 # def disconnect_database():
 #     connection.close()
+
+# COMMANDS TO CREATE TABLES
+# CREATE TABLE users (user_id int NOT NULL AUTO_INCREMENT, username varchar(55) NOT NULL, password varchar(55) NOT NULL, email varchar(55) NOT NULL, PRIMARY KEY (id));
+# CREATE TABLE profile (prof_id int NOT NULL AUTO_INCREMENT, admin varchar(10) NOT NULL,  user_id int NOT NULL, firstname varchar(55) NOT NULL, lastname varchar(55) NOT NULL, email varchar(55) NOT NULL, age int NOT NULL, mobileNumber varchar(15) NOT NULL, address varchar(100) NOT NULL, PRIMARY KEY (prof_id), FOREIGN KEY(user_id) REFERENCES users(user_id));
+# CREATE TABLE vaccine_info (vaccine_id int NOT NULL AUTO_INCREMENT, user_id int NOT NULL, slot_id int NOT NULL, PRIMARY KEY (vaccine_id), FOREIGN KEY(slot_id) REFERENCES slots(slot_id));
+# CREATE TABLE slots (slot_id int NOT NULL AUTO_INCREMENT, time_slot DATETIME NOT NULL, vaccine_count int NOT NULL, PRIMARY KEY (slot_id));
